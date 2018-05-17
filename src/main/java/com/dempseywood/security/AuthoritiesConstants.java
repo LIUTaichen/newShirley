ab11b4e8cba6e1973b8835fa87313842965d74e1
LIUTaichen <liu_taichen@hotmail.com>
2018-05-18 11:47:10 +1200

    added new roles

--- src/main/java/com/dempseywood/security/AuthoritiesConstants.java
+++ src/main/java/com/dempseywood/security/AuthoritiesConstants.java
@@ -9,6 +9,10 @@ public final class AuthoritiesConstants {
 
     public static final String USER = "ROLE_USER";
 
+    public static final String DW = "ROLE_DW";
+
+    public static final String QUATTRA = "ROLE_QUATTRA";
+
     public static final String ANONYMOUS = "ROLE_ANONYMOUS";
 
     private AuthoritiesConstants() {
