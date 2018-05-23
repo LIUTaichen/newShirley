7bdeba8dfae5e413915fb3662515cd8cc14298a4
LIUTaichen <liu_taichen@hotmail.com>
2018-05-24 09:44:52 +1200

    replace select with autocomplete for the selection of plant

--- src/main/webapp/app/customization/niggle-dw/niggle-list-dw/niggle-edit-dialog/niggle-edit-dialog.component.ts
+++ src/main/webapp/app/customization/niggle-dw/niggle-list-dw/niggle-edit-dialog/niggle-edit-dialog.component.ts
@@ -10,6 +10,8 @@ import { HttpResponse, HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { JhiEventManager, JhiAlertService } from 'ng-jhipster';
 import { MatDialogRef, MatSnackBar, MAT_DIALOG_DATA } from '@angular/material';
+import { startWith } from 'rxjs/operators/startWith';
+import { map } from 'rxjs/operators/map';
 
 @Component({
   selector: 'jhi-niggle-edit-dialog',
@@ -21,8 +23,9 @@ export class NiggleEditDialogComponent implements OnInit {
   niggle: Niggle;
   niggleForm: FormGroup;
   plants: Plant[];
-  maintenancecontractors: MaintenanceContractor[];
+  maintenanceContractors: MaintenanceContractor[];
   isSaving: boolean;
+  filteredOptions: Observable<Plant[]>;
 
   constructor(
     public snackBar: MatSnackBar,
@@ -35,7 +38,19 @@ export class NiggleEditDialogComponent implements OnInit {
     private eventManager: JhiEventManager,
     @Inject(MAT_DIALOG_DATA) public data: any) {
     this.niggle = data.niggle;
+    this.plants = data.plants;
+    this.maintenanceContractors = data.maintenanceContractors;
     this.createForm();
+    this.filteredOptions = this.niggleForm.controls.plant.valueChanges
+      .pipe(
+        startWith(''),
+        map((val) => {
+          const filtered = this.filter(val);
+          console.log(filtered.length);
+          return filtered;
+        }
+        )
+      );
     this.setOriginalValue();
   }
 
@@ -52,15 +67,15 @@ export class NiggleEditDialogComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.plantService.query()
-      .subscribe((res: HttpResponse<Plant[]>) => { this.plants = res.body; }, (res: HttpErrorResponse) => this.onError(res.message));
-    this.maintenanceContractorService.query()
-      .subscribe((res: HttpResponse<MaintenanceContractor[]>) => {
-        this.maintenancecontractors = res.body;
-        this.niggleForm.patchValue({
-          contractor: this.maintenancecontractors[0]
-        });
-      }, (res: HttpErrorResponse) => this.onError(res.message));
+    // this.plantService.query()
+    //   .subscribe((res: HttpResponse<Plant[]>) => { this.plants = res.body; }, (res: HttpErrorResponse) => this.onError(res.message));
+    // this.maintenanceContractorService.query()
+    //   .subscribe((res: HttpResponse<MaintenanceContractor[]>) => {
+    //     this.maintenancecontractors = res.body;
+    //     this.niggleForm.patchValue({
+    //       contractor: this.maintenancecontractors[0]
+    //     });
+    //   }, (res: HttpErrorResponse) => this.onError(res.message));
   }
 
   private onError(error: any) {
@@ -127,5 +142,38 @@ export class NiggleEditDialogComponent implements OnInit {
 
   compareObjects(o1: any, o2: any): boolean {
     return o1.id === o2.id;
+  }
+
+  filter(val: string): Plant[] {
+    console.log('filtering with', val);
+    let feed: string;
+    if (val === null) {
+      feed = '';
+    } else {
+      feed = val.toString().toLocaleLowerCase();
+    }
+    return this.plants.filter((option) =>
+      option.fleetId.toLowerCase().indexOf(feed) === 0
+    );
+  }
+
+  displayPlant(plant?: Plant): string | undefined {
+    return plant ? plant.fleetId : undefined;
+  }
+
+  handleLowerCaseInput() {
+    let feed: string;
+    if (this.niggleForm.value.plant && !this.niggleForm.value.plant.id) {
+      feed = this.niggleForm.value.plant.toLocaleLowerCase();
+      console.log('looking for a match with feed: ', feed);
+      for (let i = 0; i < this.plants.length; i++) {
+        const option = this.plants[i];
+        if (option.fleetId.toLowerCase() === feed) {
+          console.log('found matching record: ', option);
+          this.niggleForm.controls.plant.setValue(option);
+        }
+      }
+
+    }
   }
 }
