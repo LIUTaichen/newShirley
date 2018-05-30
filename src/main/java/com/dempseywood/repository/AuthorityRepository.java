07baa50441eab01d00d2281e5c21d6052443bd87
LIUTaichen <liu_taichen@hotmail.com>
2018-05-30 17:56:32 +1200

    Generated with JHipster 4.14.1

---
+++ src/main/java/com/dempseywood/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.dempseywood.repository;
+
+import com.dempseywood.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
