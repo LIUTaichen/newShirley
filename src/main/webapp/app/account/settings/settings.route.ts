edd7be286770fb7287683b5b52f12580c38eedd6
LIUTaichen <liu_taichen@hotmail.com>
2018-06-15 17:34:01 +1200

    allow usage of sass.

--- src/main/webapp/app/account/settings/settings.route.ts
+++ src/main/webapp/app/account/settings/settings.route.ts
@@ -7,7 +7,7 @@ export const settingsRoute: Route = {
     path: 'settings',
     component: SettingsComponent,
     data: {
-        authorities: ['ROLE_USER', 'ROLE_DW', 'ROLE_QUATTRA'],
+        authorities: ['ROLE_USER', 'ROLE_DW', 'ROLE_QUATTRA', 'ROLE_DW_READ_ONLY'],
         pageTitle: 'global.menu.account.settings'
     },
     canActivate: [UserRouteAccessService]
